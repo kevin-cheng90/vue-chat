36911c1ee13f3fa52eff25f0442195d422b3392c
Kevin Cheng <kevinhc2@uci.edu>
2021-10-13 15:24:56 -0700

    chat working, compatible with firebase 9.0+

---
+++ src/components/CreateMessage.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="container" style="margin-bottom: 30px">
+		<form @submit.prevent="createMessage">
+			<div class="form-group"> 
+				<input type="text" name="message" class="form-control" placeholder="Enter Message ..." v-model="newMessage">
+				<p class="text-danger" v-if="errorText">{{ errorText }}</p>
+				<button class="btn btn-primary" type="submit" name="action">Submit</button>
+			</div>
+			
+		</form>
+	</div>
+
+</template>
+
+<script>
+import fb from "@/firebase/init";
+
+export default {
+	name: "CreateMessage",
+	props: ["name"],
+	data() {
+		return {
+			newMessage: null,
+			errorText: null
+		}
+	},
+	methods: {
+		createMessage() {
+			if (this.newMessage) {
+				fb.collection("messages").add({
+					message: this.newMessage,
+					name: this.name,
+					timestamp: Date.now()
+				}).catch(err => {
+					console.log(err)
+				})
+				this.newMessage = null;
+				this.errorText = null;
+			} else {
+				this.errorText = "A message must be entered before submitting";
+			}
+
+		}
+	}
+}
+
+
+</script>
---
+++ src/firebase/init.js
@@ -0,0 +1,23 @@
+
+// Compatible with version 9.0+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+app.firestore().settings({ timestampsInSnapshots: true });
+
+export default app.firestore();
